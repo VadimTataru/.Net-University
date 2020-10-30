5cf5cf2acf945bbb47071cd73332dbf75f97530d
4WordsToChokeUpon <v4gum@yandex.ru>
2020-10-30 22:23:30 +0300

    firstHW
    A console app that converts days to years months and days.

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество дней: ");
+            int dayCount = Convert.ToInt32(Console.ReadLine());
+
+            int years = dayCount / 365;
+            int months = (dayCount - years * 365) / 30;
+            int days = dayCount - (years * 365 + months * 30);
+
+            Console.WriteLine($"{years} лет/года/год, {months} месяцев/месяц, {days} дней/день");
+            Console.ReadKey();
+        }
+    }
+}
